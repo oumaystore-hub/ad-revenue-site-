03c4cb768fc82a4b0eb1af361e741a5d8cb34ae7
oumaystore-hub <oumaystore@gmail.com>
2026-05-21 03:14:57 +0100

    Add ad-loader.js to load Google Ads script

---
+++ public/ads/ad-loader.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const s = document.createElement('script');
+  s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+  s.async = true;
+  s.crossOrigin = 'anonymous';
+  document.head.appendChild(s);
+});
