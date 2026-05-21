2e2c99d4904459e04f2182943c9fd2a5bcb19b9c
oumaystore-hub <oumaystore@gmail.com>
2026-05-21 14:57:04 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://oumaystore-hub.github.io',
-  base: '/ad-revenue-site/', // لاحظ: بدون شرطة في البداية إذا أردت، لكن الأفضل وضعها للتوافق
-  trailingSlash: 'always',
   outDir: './dist',
+  // لا نضع site ولا base هنا أبداً
 });
