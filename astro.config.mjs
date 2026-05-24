23ac8bfec4ca2b3d14d74ec9614c88745282b799
oumaystore-hub <oumaystore@gmail.com>
2026-05-24 19:52:55 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oumaystore-hub.github.io/ad-revenue-site',
-  integrations: [sitemap()],
   base: '/ad-revenue-site'
 });
